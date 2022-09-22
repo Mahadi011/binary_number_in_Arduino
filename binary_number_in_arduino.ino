c9c6a2dcfeac898b445268b8bc9a95f2ad2e771b
Mahadi011 <113011503+Mahadi011@users.noreply.github.com>
2022-09-22 08:40:57 +0200

    Update binary_number_in_arduino.ino

--- binary_number_in_arduino.ino
+++ binary_number_in_arduino.ino
@@ -1,5 +1,6 @@
 // C++ code
 //
+// taking pin numbers as veriable
 
 int pin1= 8;
 int pin2 = 9;
@@ -9,6 +10,9 @@ int pin5 = 12;
 int i, wait = 750;
 void setup()
 {
+  
+  // declear pin modes.
+  
   pinMode(pin1, OUTPUT);
   pinMode(pin2, OUTPUT);
   pinMode(pin3, OUTPUT);
@@ -18,13 +22,17 @@ void setup()
 
 void loop()
 {
+  
+  // frist all the LED is off
+  
  digitalWrite(pin4,LOW);
  digitalWrite(pin3,LOW);
  digitalWrite(pin2,LOW);
  digitalWrite(pin1,LOW);
 delay(wait);
 i++;
-if((i % 2) > 0) 
+  
+if((i % 2) > 0)           // to detrmine number is odd or even
 { 
   digitalWrite(pin1, HIGH); 
 } 
@@ -32,7 +40,7 @@ if((i % 2) > 0)
   {
     digitalWrite(pin1, LOW); 
   }
-if((i % 4) > 1) 
+if((i % 4) > 1)          // to detrmine number is 2 bits
 { 
   digitalWrite(pin2, HIGH); 
 } 
@@ -40,7 +48,7 @@ if((i % 4) > 1)
   { 
     digitalWrite(pin2, LOW); 
   }
-if((i % 8) > 3) 
+if((i % 8) > 3)   // determine number is 3 bits
 { 
   digitalWrite(pin3, HIGH); 
 } 
@@ -48,7 +56,7 @@ if((i % 8) > 3)
   { 
     digitalWrite(pin3, LOW); 
   }
-if((i % 16) > 7) 
+if((i % 16) > 7)    // determine number is 4 bits
 { 
   digitalWrite(pin4, HIGH); 
 } 
@@ -56,7 +64,7 @@ if((i % 16) > 7)
   { 
     digitalWrite(pin4, LOW); 
   }
-if((i%32)>15)
+if((i%32)>15)           // te determine number is 5 bits
 {
   digitalWrite(pin5,HIGH);
 }
